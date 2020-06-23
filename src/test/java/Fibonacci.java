3bc50b7b2d6640f5d3a0b5500a18da06cb5f77c3
Volodymyr Bendiukevych <volodymyrbendiukevych@Volodymyrs-MacBook-Pro.local>
2020-06-23 11:26:00 -0500

    new project practice

---
+++ src/test/java/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int firstNum=0;
+        int secondNum=1;
+
+        int []numb = {7,2,9,0,5,6,8};
+
+        for(int i=0; i<numb.length; i++) {
+            for(int k=i+1;k<numb.length; k++) {
+                if(numb[i]>numb[k]) {
+                    numb[k]=numb[i];
+                }
+                numb[i]=numb[i];
+            }
+        }
+        System.out.println(Arrays.toString(numb));
+
+    }
+
+}
+
+
